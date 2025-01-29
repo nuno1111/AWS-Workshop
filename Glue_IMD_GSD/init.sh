7a435650fe9f73a37d8a4a929a10d3ac4a0a70d6
nuno1111 <nuno1026@gmail.com>
2025-01-30 08:26:13 +0900

    EMR 소스 업로드

--- Glue_IMD_GSD/init.sh
+++ Glue_IMD_GSD/init.sh
@@ -1,11 +1,11 @@
 # Account Name 기준으로 Bucket명 설정
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --query Account --output text`
-AWS_REGION=`aws configure get region`
+# AWS_REGION=`aws configure get region`
 BUCKET_NAME=${AWS_ACCOUNT_ID}-analytics-workshop-bucket
 echo " "
-echo "export BUCKET_NAME=\"${BUCKET_NAME}\"" >> /home/ec2-user/.bashrc
-echo "export AWS_REGION=\"${AWS_REGION}\"" >> /home/ec2-user/.bashrc
-echo "export AWS_ACCOUNT_ID=\"${AWS_ACCOUNT_ID}\"" >> /home/ec2-user/.bashrc
+# echo "export BUCKET_NAME=\"${BUCKET_NAME}\"" >> /home/ec2-user/.bashrc
+# echo "export AWS_REGION=\"${AWS_REGION}\"" >> /home/ec2-user/.bashrc
+# echo "export AWS_ACCOUNT_ID=\"${AWS_ACCOUNT_ID}\"" >> /home/ec2-user/.bashrc
 echo ${AWS_ACCOUNT_ID}
 echo ${BUCKET_NAME}
 echo ${AWS_REGION}
@@ -15,3 +15,7 @@ aws s3api create-bucket --bucket ${BUCKET_NAME} --region ${AWS_REGION}
 
 # data 폴더 업로드
 aws s3 cp data/ s3://${BUCKET_NAME}/data/ --recursive
+
+# EMR 파일 업로드 
+aws s3 cp emr_source/emr_pyspark.py s3://${BUCKET_NAME}/scripts/ --recursive
+
